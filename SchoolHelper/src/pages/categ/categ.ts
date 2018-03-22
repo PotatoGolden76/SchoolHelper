09dc46f48795dac5fa2857a02a3fb2825adff978
Potato <potatogolden76@gmail.com>
2018-03-22 22:31:08 +0200

    V0.2
    Added:
    -categ page auto-fill from array info
    Planned:
    -auto-filling list of sub-domains for every category

--- SchoolHelper/src/pages/categ/categ.ts
+++ SchoolHelper/src/pages/categ/categ.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HomePage } from '../home/home';
+import { InfoPage } from '../info/info';
+import { CatPage } from '../categ/categ';
+
 @Component({
   selector: 'page-categ',
   templateUrl: 'categ.html'
 })
 export class CatPage {
 
+	cards: Array<{title: string, photo: string, component: any}>;
   	constructor(public navCtrl: NavController) {
-
+  		this.cards = [
+      		{ title: 'Matematică', photo: '../assets/imgs/maths.jpg', component: HomePage },
+      		{ title: 'Fizică', photo: '../assets/imgs/physics.jpg', component: CatPage },
+      		{ title: 'Chimie', photo: '../assets/imgs/chem.jpg', component: InfoPage}
+    	];
   	}
 
 }
